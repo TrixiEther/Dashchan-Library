510a3607c7986ca5eaa0b10ea1dd7c0cea1aa3fb
TrixiEther <trixiether@gmail.com>
2022-08-30 13:29:00 +0300

    Feature | Likes-Dislikes
    Added to the lib

--- libraries/api/src/chan/content/model/Post.java
+++ libraries/api/src/chan/content/model/Post.java
@@ -521,6 +521,33 @@ public final class Post implements Comparable<Post> {
 	}
 
 	/**
+	 * <p>Returns likes on post.</p>
+	 *
+	 * @return Likes count.
+	 */
+	public int getLikes() {
+		return BuildConfig.Private.expr();
+	}
+
+	/**
+	 * <p>Returns dislikes on post.</p>
+	 *
+	 * @return Dislikes count.
+	 */
+	public int getDislikes() {
+		return BuildConfig.Private.expr();
+	}
+
+	/**
+	 * <p>Returns whether the voting feature is enabled for the post.</p>
+	 *
+	 * @return Is voting active.
+	 */
+	public boolean isShowVotes() {
+		return BuildConfig.Private.expr();
+	}
+
+	/**
 	 * Compares this post with specified post.
 	 *
 	 * @param another Post to compare with.
